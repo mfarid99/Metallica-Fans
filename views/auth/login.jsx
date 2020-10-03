72a3c29a85a3ca2c0c31fc4360fb89d5d643062b
mfarid99 <67521156+mfarid99@users.noreply.github.com>
2020-10-02 22:35:10 -0400

    final project

--- views/auth/login.jsx
+++ views/auth/login.jsx
@@ -5,11 +5,22 @@ class Login extends React.Component {
   render() {
     return (
       <Layout title="LOGIN">
+                <div className = "editContainer"> 
+
         <form action="/auth/login" method="post">
           <input type="text" name="username" placeholder="username"/>
           <input type="text" name="password" placeholder="password"/>
           <input type="submit" value="login"/>
         </form>
+        </div>
+
+        <div className = "hetfieldsignup">
+        <img className = "mainimg" src = "https://media.npr.org/assets/img/2016/11/16/metallica_wide-d81a804be04b68eb9257504a38c9a8370374d754.jpg?s=1400"></img>
+        
+        <div className = "signuptxt">
+        <a href = "/auth/login">  
+          <h1 className = "loginpgtxt">LOGIN!!</h1></a></div>
+        </div>
       </Layout>
     );
   }
--- views/auth/signup.jsx
+++ views/auth/signup.jsx
@@ -5,11 +5,26 @@ class Index extends React.Component {
   render() {
     return (
       <Layout title="SIGNUP">
+                <div className = "editContainer"> 
+
         <form action="/auth/signup" method="post">
           <input type="text" name="username" placeholder="username" />
           <input type="text" name="password" placeholder="password" />
           <input type="submit" value="signup" />
         </form>
+        
+        </div>
+
+        <div className = "hetfieldsignup">
+        <img className = "mainimg" src = "https://www.wwaytv3.com/wp-content/uploads/2018/02/1280x960_60802P00-JHMDR.jpg"></img>
+        <div className = "signuptxt">
+        <a className = "signupscreamlink" href = "/auth/signup">  
+          <h1 className = "hetfieldscreamtxt">SIGNUP!!</h1>
+          </a>
+          </div>
+        </div>
+
+        
       </Layout>
     );
   }
